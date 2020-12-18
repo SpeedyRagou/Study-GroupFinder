017787fe6fa5f0252612979958e6c18250e03bcf
Malte <m.f.kuhlmann@gmail.com>
2020-12-18 22:28:52 +0100

    group and chat bar is now implemented. Every button has a response

--- body.js
+++ body.js
@@ -1,3 +1,14 @@
+function init(){
+    id = "gruppen";
+    var tab = document.getElementById(id);
+    tab.style.display = "block";
+
+    id = "gruppenbutton"; 
+    var tab = document.getElementById(id);
+    tab.className += " active";
+
+}
+
 function tabopen(event, id){
 
     var tabs = document.getElementsByClassName("tabinhalt");
@@ -18,4 +29,20 @@ function tabopen(event, id){
     tab.style.display = "block";
     event.currentTarget.className += " active"
 
+}
+
+function newGroup(){
+    console.log("Todo: Gruppenerstellung implementieren");
+}
+
+function search(){
+    console.log("Todo: Suche implementieren");
+}
+
+function filter(){
+    console.log("Todo: Filter implementieren");
+}
+
+function openProfil(){
+    console.log("Todo: Profil implementieren");
 }
